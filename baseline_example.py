918721fd73da25b437b4b10cef615cc52f591a49
Jesse Perla <jesseperla@gmail.com>
2022-12-13 14:40:13 -0800

    Added in quantecon dependency

--- baseline_example.py
+++ baseline_example.py
@@ -7,10 +7,12 @@ import numpy as np
 import wandb
 import warnings 
 import timeit
+import quantecon
+import econ_layers
 
+from scipy import optimize
 from torch import nn
 from torch.utils.data import DataLoader
-import econ_layers
 from econ_layers.utilities import dict_to_cpu
 from pytorch_lightning.cli import LightningCLI
 from pathlib import Path
@@ -18,12 +20,65 @@ from copy import deepcopy
 from typing import Optional
 from pytorch_lightning.loggers import WandbLogger
 
-# Local files and utilities
-import symmetry_dp.linear_policy_LQ
-
 warnings.filterwarnings(action="ignore", category=UserWarning, message="Due to class_path change from")
 
-# TODO: MOVE TO UTILITIES
+# UTILITIES
+
+# Calculates the LQ solution imposing symmetry
+# by hand in the optimization process
+def investment_equilibrium_LQ(N, params):
+    H_iv = [80.0, -0.2, 0.0]
+
+    # Equation (22)
+    B = np.zeros([N + 2, 1])
+    B[1] = 1.0
+
+    # Equation (23)
+    C_1 = np.zeros([N + 1, N + 1])
+    C_2 = np.zeros([1, N + 1])
+    C_1[np.diag_indices(N + 1)] = params.sigma
+    C_1[:, 0] = params.eta
+    C_1[0, 1] = params.sigma
+    C = np.concatenate((C_2, C_1))
+
+    # Equation (24)
+    R = np.zeros([N + 2, N + 2])
+    R[1, :] = params.alpha_1 / (2 * N)
+    R[:, 1] = params.alpha_1 / (2 * N)
+    R[1, 1] = 0.0
+    R[0, 1] = -params.alpha_0 / 2
+    R[1, 0] = -params.alpha_0 / 2
+
+    Q = params.gamma / 2
+
+    # calculating A_hat
+    def F_root(H):
+        # Equation (30)
+        H_0, H_1, H_2 = H  # H_2 not used
+
+        # Equation (21)
+        A = (H_1 / N) * np.ones([N + 2, N + 2])
+        A[np.diag_indices(N + 2)] = 1.0 - params.delta + H_1 / N
+        A[:, 0] = H_0
+        A[:, 1] = 0.0
+        A[0, :] = 0.0
+        A[1, :] = 0.0
+        A[0, 0] = 1.0
+        A[1, 1] = 1.0 - params.delta
+
+        lq = quantecon.LQ(Q, R, A, B, C, beta=params.beta)
+        P, F, d = lq.stationary_values()
+        return np.array([F[0][0], F[0][1], F[0][2]]) - np.array([-H[0], 0.0, -H[1] / N])
+
+    H_opt = optimize.root(F_root, H_iv, method="lm", options={"xtol": 1.49012e-8})
+    if not (H_opt.success):
+        sys.exit("H optimization failed to converge.")
+
+    H_hat = H_opt.x
+    if params.verbose:
+        print(f"LQ optima are: {H_hat}")
+    return H_hat[0], H_hat[1]
+
 def gauss_hermite_1D(N):
     nodes, weights = np.polynomial.hermite.hermgauss(N)
     nodes *= np.sqrt(2.0)
@@ -79,7 +134,7 @@ class InvestmentEulerBaseline(pl.LightningModule):
 
         # Solves the LQ problem to find the comparison for the baseline
         # used for comparison as well as simulation of datapoints
-        self.H_0, self.H_1 = symmetry_dp.linear_policy_LQ.investment_equilibrium_LQ(1, self.hparams)
+        self.H_0, self.H_1 = investment_equilibrium_LQ(1, self.hparams)
 
         # The "simulation_policy" function starts by using the linear_policy
         # to begin the simulation of X_t grid points.  Swaps out later if always_simulate_linear = False
