53370e0fa90b4b2cfb20a9d3d67cf65aa441e8a2
Mahdi E Kahou (Mahdi Ebrahimi Kahou) <43940086+Mekahou@users.noreply.github.com>
2023-03-30 17:26:27 -0600

    fixing labels

--- generate_figures/linear_baseline_theory_vs_predicted.py
+++ generate_figures/linear_baseline_theory_vs_predicted.py
@@ -48,11 +48,11 @@ def first_satisfying_run(summary_value, threshold, tag):
                 data = pd.DataFrame(data = get.data, columns = get.columns)
                 return(data)
 
-df_deep = first_satisfying_run("test_u_rel_error", 0.005, "baseline_deep_sets_I_run")
+df_deep = first_satisfying_run("test_u_rel_error", 0.005, "baseline_deep_sets_one_run")
 df_deep = df_deep[df_deep["ensemble"] == 0]
-df_moments = first_satisfying_run("test_u_rel_error", 0.005, "baseline_deep_moments_I_run")
+df_moments = first_satisfying_run("test_u_rel_error", 0.005, "baseline_deep_moments_one_run")
 df_moments = df_moments[df_moments["ensemble"] == 0]
-df_identity = first_satisfying_run("test_u_rel_error", 0.005, "baseline_identity_I_run")
+df_identity = first_satisfying_run("test_u_rel_error", 0.005, "baseline_identity_one_run")
 df_identity = df_identity[df_identity["ensemble"] == 0]
 
 
