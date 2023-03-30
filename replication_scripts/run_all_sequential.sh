8feb190b30ae0a2818c9a3f1a1c9447de363471b
Mahdi E Kahou (Mahdi Ebrahimi Kahou) <43940086+Mekahou@users.noreply.github.com>
2023-03-30 15:09:31 -0600

    adding L_2_deep_moments and fixing discriptions

--- replication_scripts/run_all_sequential.sh
+++ replication_scripts/run_all_sequential.sh
@@ -49,6 +49,7 @@ run_sweep_and_agent () {
 # run_sweep_and_agent "wide_256_deep_sets"
 # run_sweep_and_agent "thin_64_deep_sets"
 # run_sweep_and_agent "thin_64_identity"
+# run_sweep_and_agent "L_2_deep_moments"
 
 # # This can take a day given the massive number of variations tested
 # run_sweep_and_agent "baseline_deep_sets_N"
