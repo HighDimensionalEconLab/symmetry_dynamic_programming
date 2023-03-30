cc3e2b4d24a3537e912943bf0538c1cb57e73a47
annabellasd <annabellastolld@gmail.com>
2023-03-30 11:48:20 -0700

    Table 1

---
+++ generate_figures/linear_performance_table.py
@@ -0,0 +1,134 @@
+#import symmetry_dp
+#from symmetry_dp import experiment_row, reorganize_performance_dataframe
+import pandas as pd
+import matplotlib.pyplot as plt
+#import joypy
+from matplotlib import cm
+import yaml
+import pytorch_lightning as pl
+import os
+import wandb
+
+output_dir = "./figures"
+plot_name = "linear_performance_table"
+output_path = output_dir + "/" + plot_name + ".pdf"
+
+api = wandb.Api()
+sym_runs = "highdimensionaleconlab/symmetry_dynamic_programming"
+
+#Need to only adjust this for different runs and descriptions we are grabbing 
+#group:[description(...will want to put these in latex format), tag] 
+# with group being the key, description and tag being values
+
+networks = {
+
+'Identity': [["Baseline", "baseline_identity"], ["Thin (64 nodes)"]],
+'Moments': [["Baseline", "baseline_deep_moments"],
+               ["Moments (1,2)"],
+                ["Very Shallow (1 layer)"], 
+                ["Thin (64 nodes)"]], 
+'Deep Sets': [["Baseline", "baseline_deep_sets"],
+            ["L = 2", "L_8_deep_sets"],
+            ["L = 16", "L_16_deep_sets"], 
+            ["Shallow", "shallow_1_2_deep_sets"],
+            ["Deep 4/8", "deep_4_8_deep_sets"], 
+            ["Deep 2/4",  "deep_2_4_deep_sets"],
+            ["Thin (64 nodes)"]]
+}
+
+def summary_run(group, description, tag):
+    run_num=0 
+    d=[]
+    cols = [
+        "retcode",
+        "train_time",
+        "trainable_parameters",
+        "train_loss",
+        "test_loss",
+        "val_loss",
+        "test_u_rel_error"
+    ]
+    overall_tag = api.runs(sym_runs, filters={"tags": tag})
+    
+    for i in range(len(overall_tag)):
+        run_num+=1
+        x=[]
+        try: 
+            for col in cols:
+                x.append(overall_tag[i].summary.get(col))     
+
+        except:
+            print("THERE IS NO RUN DATA ASSOCIATED")
+        else:
+            if run_num == 1:
+                df = pd.DataFrame(x, index=cols)
+            else: 
+                df= pd.concat([df,pd.DataFrame(x, index=cols)], axis=1)
+    df = df.T.reset_index(drop=True)
+    df["trainable_parameters"] = df["trainable_parameters"] / 1000
+    df["test_u_rel_error"] = df["test_u_rel_error"] * 100
+    df["retcode"] = df[df['retcode']>=0].count()['retcode']/100
+    
+
+    ##getting the median of the dataframe and creating a new dataframe with it to return median
+    new_df = pd.DataFrame(df.quantile(0.5).to_dict(), index = [group])
+    new_df['description'] = description
+    return(new_df)
+
+##making the latex table
+
+def linear_performance_table(df):
+    df = df.rename(
+            columns={
+                "retcode":r"\shortstack{Success Rate}",
+                "elapsed_time": r"\shortstack{Time \\ (s)}",
+                "trainable_parameters": r"\shortstack{Params\\ (K)}",
+                "train_loss": r"\shortstack{Train MSE \\ ($\varepsilon$)}",
+                "test_loss": r"\shortstack{Test MSE \\ ($\varepsilon$)}",
+                "val_loss": r"\shortstack{Val MSE \\ ($\varepsilon$)}",
+                "test_u_rel_error": r"\shortstack{Policy Error\\ $\left(\frac{|u - u_{\text{ref}}|}{u_{\text{ref}}}\right)$}",
+            }
+        )
+
+    latex_str = df.to_latex(
+        multicolumn=True,
+        multirow=True,
+        formatters=[
+            "{:.2f}\%".format,
+            "{:0.0f}".format,
+            "{:.1f}".format,
+            "{:.1e}".format,
+            "{:.1e}".format,
+            "{:.1e}".format,
+            "{:.1e}".format
+        ],
+        longtable=True,
+        sparsify=True,
+        escape=False,
+    )
+    return latex_str
+
+
+first = 0
+
+#getting all the different networks and types and combining into one dataframe
+for group in networks.keys():
+    first +=1
+    for description in networks[group]:
+        try:
+            summary_run_one = summary_run(group, description[0], description[1])
+        except:
+            print("no run yet")
+        else: 
+            if first == 1:
+                summary_run_total = summary_run_one
+            else:
+                summary_run_total = pd.concat([summary_run_one,summary_run_total])
+
+summary_run_total = summary_run_total.reset_index()
+summary_run_total.rename(columns={'index': 'group'}, inplace = True)
+summary_run_total= summary_run_total.set_index(['group', 'description'])
+summary_run_total
+
+with open(output_dir + "/linear_performance_table.tex", "w") as file:
+    file.write(linear_performance_table(summary_run_total))
