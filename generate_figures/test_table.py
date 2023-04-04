cd7f27f39888c50d5b48471eb6bce193003a239c
Mahdi E Kahou (Mahdi Ebrahimi Kahou) <43940086+Mekahou@users.noreply.github.com>
2023-04-03 20:37:02 -0600

    cleaning

--- generate_figures/test_table.py
+++
@@ -1,7 +0,0 @@
-
-api = wandb.Api()
-sym_runs = "highdimensionaleconlab/symmetry_dynamic_programming"
-
-
-overall_tag = api.runs(sym_runs, filters={"tags": "baseline_deep_moments"})
-overall_tag[1].summary 
