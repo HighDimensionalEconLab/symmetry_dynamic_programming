9d95f3815f5dedf778fa4bd2263ee72fb4b279e0
Mahdi E Kahou (Mahdi Ebrahimi Kahou) <43940086+Mekahou@users.noreply.github.com>
2023-04-05 20:33:19 -0600

    overfit table code

---
+++ generate_figures/linear_overfit_table.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import cm
+import yaml
+import pytorch_lightning as pl
+import os
+import wandb
+
+output_dir = "./figures"
+
+api = wandb.Api()
+
+sym_runs = "highdimensionaleconlab/symmetry_dynamic_programming"
+tag = "deep_sets_overfit"
+
+# Downloading the results
+overall_tag = api.runs(sym_runs, filters={"tags": tag})
+run_num=0 
+cols_df = ['Number of data points', 'retcode', 'test_u_rel_error', 'test_loss', 'train_loss']
+cols = ["retcode", "test_u_rel_error", "test_loss", "train_loss"]  
+
+for i in range(len(overall_tag)):
+    x= [ ]
+    x.append(overall_tag[i].config.get("model.train_subsample_trajectories"))
+    for col in cols:
+        x.append(overall_tag[i].summary.get(col))
+    array_x = np.array(x).reshape(1,len(cols_df))
+    if i == 0:
+        df =  pd.DataFrame(array_x, columns=cols_df)
+    else: 
+        df= pd.concat([df,pd.DataFrame(array_x, columns=cols_df)], axis =0)
+    df = df.reset_index(drop=True)
+
+# Preparing the results for the table
+
+#Finding the ones that converged
+df_converge = df[df["test_u_rel_error"]< 0.01] # Convergence criteria : test_u_rel_error < 0.01
+# Calculating the succes rates
+results_df_index= [2,3,4,5]
+success_rate = []
+for i in results_df_index:
+    success = 100*len(df_converge[df_converge["Number of data points"] == i])/len(df[df["Number of data points"] == i])
+    success_rate.append(success)
+# Creating the result data frame    
+df_results = df_converge.groupby("Number of data points").median()
+df_results['test_u_rel_error'] = df_results['test_u_rel_error']*100
+df_results = df_results.drop(['retcode'], axis=1)
+df_results['success'] = success_rate
+df_results['Number of data points'] = [2,3,4,5]
+df_results = df_results[['Number of data points','success', 'train_loss', 'test_loss', 'test_u_rel_error']]
+df_results = df_results.set_index(['Number of data points'])
+# Creating the latex file
+
+
+def latex_table(df):
+    df = df.rename(
+            columns={
+                "success":r"\shortstack{Success \\(\%)}",
+                "train_loss": r"\shortstack{Train MSE \\ ($\varepsilon$)}",
+                "test_loss": r"\shortstack{Test MSE \\ ($\varepsilon$)}",
+                "test_u_rel_error": r"\shortstack{Policy Error\\ ($\epsilon_{\mathrm{rel}}$)}",
+            }
+        )
+
+    latex_str = df.to_latex(
+        multicolumn=True,
+        multirow=True,
+        formatters=[
+            "{:0.0f}\%".format,
+            "{:.1e}".format,
+            "{:.1e}".format,
+            "{:.2f}\%".format
+        ],
+        index_names =  "number of ", 
+        longtable=False,
+        sparsify=True,
+        escape=False,
+    )
+    latex_list = latex_str.splitlines()
+    #latex_list.insert(6, '\midrule')
+    latex_new = '\n'.join(latex_list)
+    return latex_new
+
+with open(output_dir + "/linear_overfit_table.tex", "w") as file:
+    file.write(latex_table(df_results))
