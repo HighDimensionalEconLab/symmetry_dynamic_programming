43bea9857fe5e90687059cbe4e0de1ba281f810a
Jesse Perla <jesseperla@gmail.com>
2023-05-12 13:54:38 -0700

    Added buffer for early stopping with train_loss

--- generalized_mean.py
+++ generalized_mean.py
@@ -34,6 +34,7 @@ class GeneralizedMean(pl.LightningModule):
         test_seed: int,
         train_data_seed: int,
         test_loss_success_threshold: float,
+        early_stopping_success_threshold: float,
         # parameters for method
         num_train_points: int,
         num_val_points: int,
@@ -186,29 +187,94 @@ class GeneralizedMean(pl.LightningModule):
             else len(self.test_data),
         )
 
-
 def log_and_save(trainer, model, train_time):
-    if model.hparams.save_test_results and trainer.log_dir is not None:
-        model.test_results.to_csv(Path(trainer.log_dir) / "test_results.csv", index=False)
     if type(trainer.logger) is WandbLogger:
-        # The calculated runtime with pytorch lightning + wandb has many fixed costs which throw off performance comparisons
-        trainer.logger.experiment.log({"train_time": train_time})
+        # Valid numeric types
+        def not_number_type(value):
+            if value is None:
+                return True
 
-        # Count and log the number of parameters with are trained in the neural network
+            if not isinstance(value, (int, float)):
+                return True
+
+            if math.isnan(value) or math.isinf(value):
+                return True
+
+            return False  # otherwise a valid, non-infinite number
+
+        # If early stopping, evaluate success
+        early_stopping_check_failed = math.nan
+        early_stopping_monitor = ""
+        early_stopping_threshold = math.nan
+        for callback in trainer.callbacks:
+            if type(callback) == pl.callbacks.early_stopping.EarlyStopping:
+                early_stopping_monitor = callback.monitor
+                early_stopping_threshold = callback.stopping_threshold
+                early_stopping_check_failed = not_number_type(
+                    cli.trainer.logger.experiment.summary[callback.monitor]
+                ) or (
+                    cli.trainer.logger.experiment.summary[callback.monitor] - model.hparams.early_stopping_success_threshold # enable buffer to fix weird early stopping bugs
+                    > callback.stopping_threshold
+                )
+                break
+
+        # Check test loss
+        if model.hparams.test_loss_success_threshold == 0:
+            test_loss_check_failed = math.nan
+        elif not_number_type(cli.trainer.logger.experiment.summary["test_loss"]) or (
+            cli.trainer.logger.experiment.summary["test_loss"]
+            > model.hparams.test_loss_success_threshold
+        ):
+            test_loss_check_failed = True
+        else:
+            test_loss_check_failed = False
+
+        # Determine convergence results
+        if (
+            early_stopping_check_failed in [False, math.nan]
+            and test_loss_check_failed in [False, math.nan]
+        ):
+            retcode = 0
+            convergence_description = "Success"
+        elif early_stopping_check_failed == True:
+            retcode = -1
+            convergence_description = "Early stopping failure"
+        elif test_loss_check_failed == True:
+            retcode = -3
+            convergence_description = "Test loss failure due to possible overfitting"
+        else:
+            retcode = -100
+            convergence_description = " Unknown failure"
+
+        # Log all calculated results
         trainable_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        trainer.logger.experiment.log({"train_time": train_time})
+        trainer.logger.experiment.log({"early_stopping_monitor": early_stopping_monitor})
+        trainer.logger.experiment.log({"early_stopping_threshold": early_stopping_threshold})
+        trainer.logger.experiment.log({"early_stopping_check_failed": early_stopping_check_failed})
+        trainer.logger.experiment.log({"test_loss_check_failed": test_loss_check_failed})
         trainer.logger.experiment.log({"trainable_parameters": trainable_parameters})
+        trainer.logger.experiment.log({"retcode": retcode})
+        trainer.logger.experiment.log({"convergence_description": convergence_description})
 
-        # Set objective for hyperparameter optimization.
-        hpo_objective_value = dict(cli.trainer.logger.experiment.summary)[
-            model.hparams.hpo_objective_name
-        ]
+        # Set objective for hyperparameter optimization
+        # Objective value given in the settings, or empty
+        if model.hparams.hpo_objective_name is not None:
+            hpo_objective_value = dict(cli.trainer.logger.experiment.summary)[
+                model.hparams.hpo_objective_name
+            ]
+        else:
+            hpo_objective_value = math.nan
 
-        if model.hparams.always_log_hpo_objective:
-            trainer.logger.experiment.log({"hpo_objective": hpo_objective_value})
-        elif trainer.current_epoch < trainer.max_epochs:
+        if model.hparams.always_log_hpo_objective or retcode >= 0:
             trainer.logger.experiment.log({"hpo_objective": hpo_objective_value})
         else:
             trainer.logger.experiment.log({"hpo_objective": math.nan})
+
+        # Save test results
+        trainer.logger.log_text(
+            key="test_results", dataframe=trainer.model.test_results
+        )  # Saves on wandb for querying later
 
         # save the summary statistics in a file
         if model.hparams.save_metrics and trainer.log_dir is not None:
@@ -218,12 +284,10 @@ def log_and_save(trainer, model, train_time):
 
         if model.hparams.print_metrics:
             print(dict(cli.trainer.logger.experiment.summary))
-
-        # Store the test_results field from model if it exists
-        if hasattr(model, "test_results"):
-            trainer.logger.log_text(
-                key="test_results", dataframe=trainer.model.test_results
-            )  # Saves on wandb for querying later
+        return
+    else:  # almost no features enabled for other loggers. Could refactor later
+        if model.hparams.save_test_results and trainer.log_dir is not None:
+            model.test_results.to_csv(Path(trainer.log_dir) / "test_results.csv", index=False)
 
 
 if __name__ == "__main__":
