e880f426e94c111a2f0d42a4cc635e48bf63d4bc
Jesse Perla <jesseperla@gmail.com>
2023-05-12 13:59:29 -0700

    Generalized mean update (#19)
    * Updated the yaml and signature
    
    * Done up to the log and save function
    
    * Test results now working on CPU
    
    * Added buffer for early stopping with train_loss

--- generalized_mean.py
+++ generalized_mean.py
@@ -7,11 +7,12 @@ import numpy as np
 import wandb
 import timeit
 import econ_layers
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
-from econ_layers.utilities import dict_to_cpu
 from pytorch_lightning.cli import LightningCLI
 from pathlib import Path
 from pytorch_lightning.loggers import WandbLogger
+from torch.utils.data import TensorDataset
 
 
 class GeneralizedMean(pl.LightningModule):
@@ -30,6 +31,10 @@ class GeneralizedMean(pl.LightningModule):
         print_metrics: bool,
         save_metrics: bool,
         save_test_results: bool,
+        test_seed: int,
+        train_data_seed: int,
+        test_loss_success_threshold: float,
+        early_stopping_success_threshold: float,
         # parameters for method
         num_train_points: int,
         num_val_points: int,
@@ -49,15 +54,16 @@ class GeneralizedMean(pl.LightningModule):
 
     def training_step(self, batch, batch_idx):
         x, y = batch
-        residuals = y - self(x)
-        loss = (residuals**2).sum() / len(residuals)
+        y = y.unsqueeze(1)  # to enable broadcasting of self(x)
+        loss = F.mse_loss(self(x), y, reduction="mean")
         self.log("train_loss", loss)
         return loss
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
+        y = y.unsqueeze(1)  # to enable broadcasting of self(x)
         residuals = y - self(x)
-        loss = (residuals**2).sum() / len(residuals)
+        loss = F.mse_loss(self(x), y, reduction="mean")
 
         rel_error = torch.mean(torch.abs(residuals) / torch.abs(y))
         abs_error = torch.mean(torch.abs(residuals))
@@ -68,9 +74,10 @@ class GeneralizedMean(pl.LightningModule):
 
     def test_step(self, batch, batch_idx):
         x, y_f = batch
+        y_f = y_f.unsqueeze(1)  # to enable broadcasting of self(x)
         y = self(x)
         residuals = y_f - y
-        loss = (residuals**2).sum() / len(residuals)
+        loss = F.mse_loss(y_f, y, reduction="mean")
         rel_error = torch.abs(y_f - y) / torch.abs(y_f)
         abs_error = torch.abs(y_f - y)
 
@@ -78,15 +85,13 @@ class GeneralizedMean(pl.LightningModule):
             [
                 self.test_results,
                 pd.DataFrame(
-                    dict_to_cpu(
-                        {
-                            "x_norm": x.norm(dim=1),  # x is too large to store
-                            "f_x": y_f,
-                            "f_hat_x": y,
-                            "rel_error": rel_error,
-                            "abs_error": abs_error,
-                        }
-                    )
+                    {
+                        "x_norm": x.norm(dim=1).cpu().numpy().tolist(),  # x is too large to store
+                        "f_x": y_f.squeeze().cpu().numpy().tolist(),
+                        "f_hat_x": y.squeeze().cpu().numpy().tolist(),
+                        "rel_error": rel_error.squeeze().cpu().numpy().tolist(),
+                        "abs_error": abs_error.squeeze().cpu().numpy().tolist(),
+                    }
                 ),
             ]
         )
@@ -95,70 +100,181 @@ class GeneralizedMean(pl.LightningModule):
         self.log("test_abs_error", abs_error.mean(), prog_bar=True)
 
     # simulate DGP
-    def simulate_data(self, num_points):
-        simulated_data = []
+    def simulate_data(self, num_points, generator=None):
+        X = torch.empty(num_points, self.hparams.N, device=self.device, dtype=self.dtype)
         for i in range(0, num_points):
-            a_i = np.random.uniform(self.hparams.a_min, self.hparams.a_max)
-            if self.hparams.X_distribution=="normal":
-                X = torch.normal(a_i, self.hparams.std, size=(self.hparams.N,))
-            elif self.hparams.X_distribution=="uniform":
-                d = self.hparams.std * math.sqrt(3) # ensures std is correct
-                X = torch.rand(self.hparams.N) * 2 * d + a_i - d # uniform in [a_i - d, a_i + d]
+            a_i = torch.empty(1).uniform_(
+                self.hparams.a_min, self.hparams.a_max, generator=generator
+            )
+            if self.hparams.X_distribution == "normal":
+                X[i] = torch.normal(
+                    a_i,
+                    self.hparams.std,
+                    size=(self.hparams.N,),
+                    device=self.device,
+                    dtype=self.dtype,
+                    generator=generator,
+                )
+            elif self.hparams.X_distribution == "uniform":
+                d = self.hparams.std * math.sqrt(3)  # ensures std is correct
+                X[i] = (
+                    torch.rand(
+                        self.hparams.N, device=self.device, dtype=self.dtype, generator=generator
+                    )
+                    * 2
+                    * d
+                    + a_i
+                    - d
+                )  # uniform in [a_i - d, a_i + d]
             else:
                 raise ValueError("Distribution not supported")
-            y = X.pow(self.hparams.p).mean().pow(1 / self.hparams.p)  # generalized mean
-            simulated_data.append((X, y.unsqueeze(0)))
-        return simulated_data
 
-    # At this point, the code is running local to the GPU/etc.
+        Y = (
+            X.pow(self.hparams.p).mean(dim=1).pow(1 / self.hparams.p)
+        )  # generalized mean  Doing mean over each row
+        return X, Y
+
     def setup(self, stage):
-        self.train_data = self.simulate_data(self.hparams.num_train_points)
-        self.val_data = self.simulate_data(self.hparams.num_val_points)
-        self.test_data = self.simulate_data(self.hparams.num_test_points)
-        self.test_results = pd.DataFrame()
+        if stage == "fit" or stage is None:
+            if self.hparams.train_data_seed > 0:
+                generator = torch.Generator(device=self.device)
+                generator.manual_seed(self.hparams.train_data_seed)
+            else:
+                generator = None  # otherwise use default RNG
+
+            # self.train_data = self.old_simulate_data(self.hparams.num_train_points)
+            X, Y = self.simulate_data(self.hparams.num_train_points, generator=generator)
+            self.train_data = TensorDataset(X, Y)
+
+            if self.hparams.num_val_points > 0:
+                X, Y = self.simulate_data(self.hparams.num_val_points, generator=generator)
+                self.val_data = TensorDataset(X, Y)
+            else:
+                self.val_data = []
+        if stage == "test":
+            if self.hparams.test_seed > 0:
+                generator = torch.Generator(device=self.device)
+                generator.manual_seed(self.hparams.test_seed)
+            else:
+                generator = None  # otherwise use default RNG
+
+            X, Y = self.simulate_data(self.hparams.num_test_points, generator=generator)
+            self.test_data = TensorDataset(X, Y)
+            self.test_results = pd.DataFrame()
 
     def train_dataloader(self):
         return DataLoader(
             self.train_data,
-            batch_size=self.hparams.batch_size,
+            batch_size=self.hparams.batch_size
+            if self.hparams.batch_size > 0
+            else len(self.train_data),
             shuffle=self.hparams.shuffle_training,
         )
 
     def val_dataloader(self):
         return DataLoader(
             self.val_data,
-            batch_size=self.hparams.batch_size,
+            batch_size=self.hparams.batch_size
+            if self.hparams.batch_size > 0
+            else len(self.val_data),
         )
 
     def test_dataloader(self):
         return DataLoader(
             self.test_data,
-            batch_size=self.hparams.batch_size,
+            batch_size=self.hparams.batch_size
+            if self.hparams.batch_size > 0
+            else len(self.test_data),
         )
 
-
 def log_and_save(trainer, model, train_time):
-    if model.hparams.save_test_results and trainer.log_dir is not None:
-        model.test_results.to_csv(Path(trainer.log_dir) / "test_results.csv", index=False)
     if type(trainer.logger) is WandbLogger:
-        # The calculated runtime with pytorch lightning + wandb has many fixed costs which throw off performance comparisons
-        trainer.logger.experiment.log({"train_time": train_time})
+        # Valid numeric types
+        def not_number_type(value):
+            if value is None:
+                return True
 
-        # Count and log the number of parameters with are trained in the neural network
+            if not isinstance(value, (int, float)):
+                return True
+
+            if math.isnan(value) or math.isinf(value):
+                return True
+
+            return False  # otherwise a valid, non-infinite number
+
+        # If early stopping, evaluate success
+        early_stopping_check_failed = math.nan
+        early_stopping_monitor = ""
+        early_stopping_threshold = math.nan
+        for callback in trainer.callbacks:
+            if type(callback) == pl.callbacks.early_stopping.EarlyStopping:
+                early_stopping_monitor = callback.monitor
+                early_stopping_threshold = callback.stopping_threshold
+                early_stopping_check_failed = not_number_type(
+                    cli.trainer.logger.experiment.summary[callback.monitor]
+                ) or (
+                    cli.trainer.logger.experiment.summary[callback.monitor] - model.hparams.early_stopping_success_threshold # enable buffer to fix weird early stopping bugs
+                    > callback.stopping_threshold
+                )
+                break
+
+        # Check test loss
+        if model.hparams.test_loss_success_threshold == 0:
+            test_loss_check_failed = math.nan
+        elif not_number_type(cli.trainer.logger.experiment.summary["test_loss"]) or (
+            cli.trainer.logger.experiment.summary["test_loss"]
+            > model.hparams.test_loss_success_threshold
+        ):
+            test_loss_check_failed = True
+        else:
+            test_loss_check_failed = False
+
+        # Determine convergence results
+        if (
+            early_stopping_check_failed in [False, math.nan]
+            and test_loss_check_failed in [False, math.nan]
+        ):
+            retcode = 0
+            convergence_description = "Success"
+        elif early_stopping_check_failed == True:
+            retcode = -1
+            convergence_description = "Early stopping failure"
+        elif test_loss_check_failed == True:
+            retcode = -3
+            convergence_description = "Test loss failure due to possible overfitting"
+        else:
+            retcode = -100
+            convergence_description = " Unknown failure"
+
+        # Log all calculated results
         trainable_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        trainer.logger.experiment.log({"train_time": train_time})
+        trainer.logger.experiment.log({"early_stopping_monitor": early_stopping_monitor})
+        trainer.logger.experiment.log({"early_stopping_threshold": early_stopping_threshold})
+        trainer.logger.experiment.log({"early_stopping_check_failed": early_stopping_check_failed})
+        trainer.logger.experiment.log({"test_loss_check_failed": test_loss_check_failed})
         trainer.logger.experiment.log({"trainable_parameters": trainable_parameters})
+        trainer.logger.experiment.log({"retcode": retcode})
+        trainer.logger.experiment.log({"convergence_description": convergence_description})
 
-        # Set objective for hyperparameter optimization.
-        hpo_objective_value = dict(cli.trainer.logger.experiment.summary)[
-            model.hparams.hpo_objective_name
-        ]
+        # Set objective for hyperparameter optimization
+        # Objective value given in the settings, or empty
+        if model.hparams.hpo_objective_name is not None:
+            hpo_objective_value = dict(cli.trainer.logger.experiment.summary)[
+                model.hparams.hpo_objective_name
+            ]
+        else:
+            hpo_objective_value = math.nan
 
-        if model.hparams.always_log_hpo_objective:
-            trainer.logger.experiment.log({"hpo_objective": hpo_objective_value})
-        elif trainer.current_epoch < trainer.max_epochs:
+        if model.hparams.always_log_hpo_objective or retcode >= 0:
             trainer.logger.experiment.log({"hpo_objective": hpo_objective_value})
         else:
             trainer.logger.experiment.log({"hpo_objective": math.nan})
+
+        # Save test results
+        trainer.logger.log_text(
+            key="test_results", dataframe=trainer.model.test_results
+        )  # Saves on wandb for querying later
 
         # save the summary statistics in a file
         if model.hparams.save_metrics and trainer.log_dir is not None:
@@ -168,12 +284,10 @@ def log_and_save(trainer, model, train_time):
 
         if model.hparams.print_metrics:
             print(dict(cli.trainer.logger.experiment.summary))
-
-        # Store the test_results field from model if it exists
-        if hasattr(model, "test_results"):
-            trainer.logger.log_text(
-                key="test_results", dataframe=trainer.model.test_results
-            )  # Saves on wandb for querying later
+        return
+    else:  # almost no features enabled for other loggers. Could refactor later
+        if model.hparams.save_test_results and trainer.log_dir is not None:
+            model.test_results.to_csv(Path(trainer.log_dir) / "test_results.csv", index=False)
 
 
 if __name__ == "__main__":
