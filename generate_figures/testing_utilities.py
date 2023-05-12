38377ad7d39c9548538ac9dc7bd6bafa3bf065ea
Jesse Perla <jesseperla@gmail.com>
2023-05-11 23:03:42 -0700

    Completed.  Ready for testing

--- generate_figures/testing_utilities.py
+++ generate_figures/testing_utilities.py
@@ -5,15 +5,35 @@ from utilities import get_results_by_tag
 api = wandb.Api()
 project = "highdimensionaleconlab/symmetry_dynamic_programming"
 
-df_nu_150 = get_results_by_tag(api, project,"deep_sets_nonlinear_nu_150_one_run")
-print(df_nu_150["id"].nunique()) # counts the number of unique, I think?
-baseline_deep_moments = get_results_by_tag(api, project,"baseline_deep_moments", max_runs = 3)
-baseline_deep_moments_no_stuff = get_results_by_tag(api, project,"baseline_deep_moments", max_runs = 2, get_summary = False, get_config = False)
-print(baseline_deep_moments["id"].nunique()) # check unique, I think?
+df = get_results_by_tag(api, project,"deep_sets_nonlinear_nu_150_one_run")
+assert(df.id.nunique() == 1) # i.e, one run
+assert(df.retcode[0] == 0) # access the retcode of the first row, which is the only one here
 
-#for one runs
-df_nu_150_test_results = get_results_by_tag(api, project,"deep_sets_nonlinear_nu_150_one_run", test_results=True)
+dict_res = df.to_dict(orient='index')[0] # as a dictionary if only one row
+assert(dict_res["retcode"] == 0) # dict don't support dict_res.retcode
 
-#for multiple artifact ones I stopped it after grabbing 5
-baseline_deep_moments_test_results = get_results_by_tag(api, project,"baseline_deep_moments", test_results=True)
-baseline_deep_moments = get_results_by_tag(api, project,"baseline_deep_moments")
+# only get a subset of stuff.  Summary is by default, config and test_results are optional
+df = get_results_by_tag(api, project,"baseline_deep_moments", max_runs = 2, get_summary = False, get_config = False)
+assert(df.id.nunique() == 2)
+assert(len(df.columns) == 2) # id and name always there
+
+df = get_results_by_tag(api, project,"baseline_deep_moments", max_runs = 40)
+assert(df.id.nunique() == 40)
+df = df[df.retcode==0] # only keep successful runs
+print(df.val_loss.mean()) # conditional on success
+
+df = get_results_by_tag(api, project,"deep_sets_nonlinear_nu_150_one_run", get_test_results=True, get_summary = False)
+assert(df.id.nunique() == 1) # only one artifact
+assert(df.t.nunique() == 64)
+df.drop(columns=["id", "name"], inplace=True) # given single run could drop to clean up clutter, but not really
+df.set_index(["ensemble", "t"], inplace=True) # can index as we see fit.
+
+#This is a big one and includes summary statistics.  Some examples below
+df = get_results_by_tag(api, project,"baseline_deep_moments", get_test_results=True, max_runs = 10)
+assert(df.id.nunique() == 10)
+assert(df.t.nunique() == 64)
+df.set_index(["id", "ensemble", "t"], inplace=True) # can index as we see fit.
+df = df[df.retcode==0] #e.g. filter only successful runs
+df.groupby("t").mean(numeric_only=True)  # groupings, etc.
+df.groupby("t").quantile([0.95, 0.5, 0.05],numeric_only=True).u_rel_error # for example...
+df.groupby("ensemble").mean(numeric_only=True).u_rel_error # across all t and all runs...
--- generate_figures/utilities.py
+++ generate_figures/utilities.py
@@ -18,17 +18,25 @@ def plot_params(figsize, fontsize, ticksize):
 
 
 
-def get_results_by_tag(api, project, tag, get_summary = True, get_config = False, get_test_results = False, max_runs = 1000):
-    runs = api.runs(project, filters={"tags": tag})
-    
-    df = pd.DataFrame()
+def get_results_by_tag(api, project, tag, get_summary = True, get_config = False, get_test_results = False, max_runs = 1000, drop_summary_cols = ["test_results", "_wandb"], drop_config_cols = []):
+
+    runs = api.runs(project, filters={"tags": tag})    
+    df = pd.DataFrame() # will concatenate
+
     for i in range(min(len(runs), max_runs)):
-        id = runs[i].id
-        cols = {'id': id, 'name': runs[i].name}
+        run = runs[i]
+        id = run.id
+        cols = {'id': id, 'name': run.name}
         if get_summary:
-            cols.update(dict(runs[i].summary))
+            cols.update(dict(run.summary))
+            for col_name in drop_summary_cols: # dropping details which don't fit in dataframes well
+                if cols.get(col_name) is not None: 
+                    del cols[col_name]
         if get_config:
-            cols.update(dict(runs[i].config))        
+            cols.update(dict(run.config))       
+            for col_name in drop_config_cols:
+                if cols.get(col_name) is not None: 
+                    del cols[col_name]             
  
         # Conditionally get the test results or just directly add the new values for the columns
         if get_test_results:
@@ -36,23 +44,13 @@ def get_results_by_tag(api, project, tag, get_summary = True, get_config = False
             test_results = api.artifact(str(reference_path)).get("test_results")
             run_data = pd.DataFrame(data = test_results.data, columns = test_results.columns)
 
-            # Add columns across everything in the test_results
+            # Add columns across everything for dropped columns.  Repetition but allows for indexing later
             for k, v in cols.items():
                 run_data[k] = v
         else:
-            # Create a dataframe with one row from the columns
+            # Create a data frame with one row from the columns
             run_data = pd.DataFrame({k: [v] for k, v in cols.items()})
 
         df = pd.concat([df, run_data], ignore_index=True)
         
     return df
-
-
-
-    # for all runs
-    # always add in the run name and id, etc.  as coulmns
-    # Then if summary = True, merge in the whole summary info
-    # if config = True, merge in the whole config 
-    # if test_resulst = False, just add that whole set of coluimns as a row
-           # else add get the test_results, then add in those columns to all of the test results then concatentate the whole dataframe.
-#Lets put this in a few clean branch off of main, add in a test_get_results.py function to the plotting which lets us play with it.
